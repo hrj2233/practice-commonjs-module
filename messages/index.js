dd590e5e3bdc138c67c6f9a9663b93a5cf159f1a
hrj2233 <hrj2233@gmail.com>
2022-05-20 01:23:16 +0900

    complete commonjs

--- messages/index.js
+++ messages/index.js
@@ -1,3 +1,7 @@
+const giveMessageToMrsPotato = require("../send-messages/give-message-to-mrs-potato");
 const message1 = "You are looking very spudly today!";
 const message2 = "There's a good deal on mattresses tomorrow.";
 const message3 = "Do you like to do things in unnecessarily complicated ways?";
+giveMessageToMrsPotato(message1);
+giveMessageToMrsPotato(message2);
+giveMessageToMrsPotato(message3);
